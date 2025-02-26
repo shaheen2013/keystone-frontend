cd99b8b51be626f85ca19f5671213802d296134d
Sadman Sakib <ssakib4040@gmail.com>
2025-02-26 16:49:32 +0600

    margin removed from footer brand

--- components/partials/Footer.tsx
+++ components/partials/Footer.tsx
@@ -10,7 +10,7 @@ export default function Footer() {
       <div className="container lg:py-24 py-12 mx-auto flex  lg:flex-row md:flex-nowrap flex-wrap flex-col gap-6">
         {/* left */}
         <div className="xl:w-80 lg:w-60">
-          <Link href="/" className="lg:mb-6 mb-5 block -ml-2">
+          <Link href="/" className="lg:mb-6 mb-5 block">
             <Image
               src="/icons/brand-logo.svg"
               alt="logo"
