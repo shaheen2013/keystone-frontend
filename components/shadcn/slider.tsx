29a4a31407db56155abb798d83a6386b14d2f7c7
Sadman Sakib <ssakib4040@gmail.com>
2025-02-26 19:23:11 +0600

    Accesibility In progress

---
+++ components/shadcn/slider.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import * as React from "react";
+import * as SliderPrimitive from "@radix-ui/react-slider";
+
+import { cn } from "@/lib/utils";
+
+const Slider = React.forwardRef<
+  React.ElementRef<typeof SliderPrimitive.Root>,
+  React.ComponentPropsWithoutRef<typeof SliderPrimitive.Root>
+>(({ className, ...props }, ref) => (
+  <SliderPrimitive.Root
+    ref={ref}
+    className={cn(
+      "relative flex w-full touch-none select-none items-center",
+      className
+    )}
+    {...props}
+  >
+    <SliderPrimitive.Track className="relative h-1 w-full grow overflow-hidden rounded-full bg-gray-3">
+      <SliderPrimitive.Range className="absolute h-full" />
+    </SliderPrimitive.Track>
+    <SliderPrimitive.Thumb className="block rounded-full transition-colors disabled:pointer-events-none disabled:opacity-50 bg-secondary-6 w-3 h-5 cursor-pointer" />
+  </SliderPrimitive.Root>
+));
+Slider.displayName = SliderPrimitive.Root.displayName;
+
+export { Slider };
