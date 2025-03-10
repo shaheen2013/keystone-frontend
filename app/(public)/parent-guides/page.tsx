f7728243622c9028c1a7ca118ac7721524bfc3d9
rasedul <74731905+rasedul99@users.noreply.github.com>
2025-03-10 11:02:06 +0600

    Inspiring Stories from Our Community component added

--- app/(public)/parent-guides/page.tsx
+++ app/(public)/parent-guides/page.tsx
@@ -7,6 +7,8 @@ import { downloadToolkits, educationPlans, guides, heroData } from "./constant";
 import Guides from "./components/guides";
 import EducationPlans from "./components/education-plans";
 import DownloadToolkits from "./components/download-toolkits";
+import Testimonials from "@/components/partials/testimonials";
+import { testimonialsData } from "@/static/homepage";
 
 export const metadata: Metadata = {
   title: "Service Details | Keystone",
@@ -21,6 +23,10 @@ export default function ParentGuides() {
       <Guides data={guides} />
       <EducationPlans data={educationPlans} />
       <DownloadToolkits data={downloadToolkits} />
+      <Testimonials
+        data={testimonialsData}
+        classes={{ root: "bg-primary-2", card: "bg-white" }}
+      />
       <GetTouch data={contactInfo} />
     </>
   );
