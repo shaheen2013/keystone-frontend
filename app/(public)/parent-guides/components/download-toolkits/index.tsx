0a7fc96ccade4c8ac96a7d1f106cd37bfe703b29
rasedul <74731905+rasedul99@users.noreply.github.com>
2025-03-10 09:53:35 +0600

    wip Download Toolkits

---
+++ app/(public)/parent-guides/components/download-toolkits/index.tsx
@@ -0,0 +1,10 @@
+const DownloadToolkits = ({ data }: { data: any }) => {
+  const { title, description, keyPoints, image } = data;
+  return (
+    <section className="py-12 md:py-28 bg-primary-2">
+      <div className="container"></div>
+    </section>
+  );
+};
+
+export default DownloadToolkits;
