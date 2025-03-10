b9c19b1b262f5a388b40d9f29a89d361d60993c7
rasedul <74731905+rasedul99@users.noreply.github.com>
2025-03-10 12:58:15 +0600

    wip faqs

---
+++ app/(public)/faqs/page.tsx
@@ -0,0 +1,23 @@
+import { Metadata } from "next";
+import { heroData } from "../events/constant";
+import CenteredHero from "@/components/partials/Hero/centered-hero";
+import Support from "@/components/partials/support";
+import { supportSectionData } from "@/static/homepage";
+import Faqs from "./components/faqs";
+import { faqsData } from "./constant";
+
+export const metadata: Metadata = {
+  title: "FAQs | Keystone",
+  description:
+    "A platform for online communities, the Disability Platform, and Atypical Advantage",
+};
+
+export default function FaqsPage() {
+  return (
+    <>
+      <CenteredHero data={heroData} />
+      <Faqs data={faqsData} />
+      <Support data={supportSectionData} />
+    </>
+  );
+}
