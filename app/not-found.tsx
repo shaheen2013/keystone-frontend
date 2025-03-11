934170de6103c443848935a6e08927389342e321
Sadman Sakib <ssakib4040@gmail.com>
2025-03-11 15:48:33 +0600

    not found page created

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,5 +1,36 @@
 import React from "react";
+import Image from "next/image";
+
+import Footer from "@/components/partials/Footer";
+import Header from "@/components/partials/Header";
+import { Button } from "@/components/shadcn/button";
+import Link from "next/link";
 
 export default function NotFound() {
-  return <div className="">404 not found</div>;
+  return (
+    <div className="">
+      <Header />
+
+      <main className="h-[calc(100vh-100px)] container mx-auto max-w-[620px] flex flex-col items-center justify-center px-4">
+        <Image
+          src="/assets/not-found.svg"
+          alt="404"
+          width={600}
+          height={600}
+          className="mb-20"
+        />
+
+        <p className="text-gray-9 font-semibold text-center mb-12">
+          The page you are trying to access doesn't exist or has been moved. Try
+          going back to our homepage.
+        </p>
+
+        <Button variant="secondary" asChild>
+          <Link href="/">Go to homepage</Link>
+        </Button>
+      </main>
+
+      <Footer />
+    </div>
+  );
 }
