9d89659571d8586e9d5b2d79e5272098b21a5a23
Sadman Sakib <ssakib4040@gmail.com>
2025-03-11 11:06:07 +0600

    favicon added

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,6 +11,9 @@ export const metadata: Metadata = {
   title: "Keystone | A Place for Connection & Support",
   description:
     "Keystone is a space where everyone can connect, engage, and find support. Join a welcoming community designed to make life easier and more meaningful.",
+  icons: {
+    icon: "/icons/favicon.ico",
+  },
 };
 
 export default function RootLayout({
