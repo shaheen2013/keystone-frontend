c6dedb21fd21f630b8500d0203ffaeffd62fdcfc
Sadman Sakib <ssakib4040@gmail.com>
2025-03-11 14:25:47 +0600

    Carousel completed

--- components/partials/AuthLeftSidebar/index.tsx
+++ components/partials/AuthLeftSidebar/index.tsx
@@ -3,14 +3,13 @@
 import Image from "next/image";
 import classNames from "classnames";
 import { useEffect, useState } from "react";
+import { ArrowLeft, ArrowRight } from "lucide-react";
 
 import {
   Carousel,
-  CarouselContent,
-  CarouselItem,
-  CarouselNext,
-  CarouselPrevious,
   CarouselApi,
+  CarouselItem,
+  CarouselContent,
 } from "@/components/partials/auth-carousel";
 import { carouselContent } from "@/static/carousel";
 
@@ -19,7 +18,9 @@ export default function AuthLeftSidebar({ className }: { className: string }) {
   const [current, setCurrent] = useState(0);
   const [count, setCount] = useState(0);
 
-  const clx = classNames("", className);
+  const clx = classNames("relative", className);
+
+  const content = carouselContent[current - 1];
 
   useEffect(() => {
     if (!api) {
@@ -52,6 +53,84 @@ export default function AuthLeftSidebar({ className }: { className: string }) {
           ))}
         </CarouselContent>
       </Carousel>
+
+      {/* information */}
+      <div className="absolute bottom-0 left-0 right-0 backdrop-blur-lg bg-[#141F1FB2] p-8">
+        {/* star */}
+        <div className="flex items-center space-x-1 mb-4">
+          <Image
+            src="/icons/star.svg"
+            alt="star"
+            width={20}
+            height={20}
+            className="h-5 w-5"
+          />
+
+          <Image
+            src="/icons/star.svg"
+            alt="star"
+            width={20}
+            height={20}
+            className="h-5 w-5"
+          />
+
+          <Image
+            src="/icons/star.svg"
+            alt="star"
+            width={20}
+            height={20}
+            className="h-5 w-5"
+          />
+
+          <Image
+            src="/icons/star.svg"
+            alt="star"
+            width={20}
+            height={20}
+            className="h-5 w-5"
+          />
+
+          <Image
+            src="/icons/star.svg"
+            alt="star"
+            width={20}
+            height={20}
+            className="h-5 w-5"
+          />
+        </div>
+
+        {/* description */}
+        <p className="text-white font-semibold text-3xl mb-8">
+          {content?.description}
+        </p>
+
+        {/* name, role, arrow */}
+        <div className="flex justify-between">
+          {/* name, role */}
+          <div className="">
+            <p className="text-white font-semibold text-xl mb-1">
+              {content?.name}
+            </p>
+            <p className="text-white text-base">{content?.role}</p>
+          </div>
+
+          {/* arrow */}
+          <div className="flex gap-x-8">
+            <button
+              className="border border-white h-14 w-14 rounded-full flex items-center justify-center"
+              onClick={() => api?.scrollPrev()}
+            >
+              <ArrowLeft size={24} stroke="#fff" />
+            </button>
+            <button
+              className="border border-secondary-6 h-14 w-14 rounded-full flex items-center justify-center bg-secondary-6"
+              onClick={() => api?.scrollNext()}
+            >
+              <ArrowRight size={24} stroke="#fff" />
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- static/carousel.ts
+++ static/carousel.ts
@@ -20,7 +20,7 @@ export const carouselContent: CarouselContent[] = [
     name: "John Doe",
     role: "Father of a child with autism",
     description:
-      "“Keystone Ability Support has been a game-changer for our family. Their guidance through the special education process gave us confidence and clarity. ",
+      "Keystone Ability Support has been an invaluable resource for our family. Their dedicated team helped us navigate challenges, providing us support to ensure a brighter future",
     ratings: 5,
     image: "/assets/auth/carousel/item-2.png",
   },
