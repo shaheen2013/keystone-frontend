be4eb31094946aca7a6a454ccb2a4eab61d72028
Sadman Sakib <ssakib4040@gmail.com>
2025-03-12 12:30:50 +0600

    Overview in progress

--- app/(public)/profile/overview/page.tsx
+++ app/(public)/profile/overview/page.tsx
@@ -2,8 +2,12 @@ import React from "react";
 
 export default function AccountOverview() {
   return (
-    <div>
-      <p>Profile Overview</p>
+    <div className="bg-primary-1 rounded-2xl">
+      <div className="font-semibold text-2xl py-6 px-8 bg-primary-2 rounded-t-2xl">
+        Profile Overview
+      </div>
+
+      {/* profile/upload */}
     </div>
   );
 }
