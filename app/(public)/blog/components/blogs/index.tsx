b6c5c392fa49ea6184aef4b321faf95d1510df8f
rasedul <74731905+rasedul99@users.noreply.github.com>
2025-03-13 10:46:05 +0600

    created paginationWrapper component

--- app/(public)/blog/components/blogs/index.tsx
+++ app/(public)/blog/components/blogs/index.tsx
@@ -14,18 +14,11 @@ import {
 import { Input } from "@/components/shadcn/input";
 import { cn } from "@/lib/utils";
 import { Check } from "lucide-react";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
 import { useDebounceCallback } from "usehooks-ts";
-import {
-  Pagination,
-  PaginationContent,
-  PaginationEllipsis,
-  PaginationItem,
-  PaginationLink,
-  PaginationNext,
-  PaginationPrevious,
-} from "@/components/shadcn/pagination";
+
+import PaginationWrapper from "@/components/partials/pagination-wrapper";
 
 const Blogs = ({ data }: { data: any }) => {
   const router = useRouter();
@@ -37,10 +30,6 @@ const Blogs = ({ data }: { data: any }) => {
   );
   const [page, setPage] = useState(searchParams.get("page") || 1);
 
-  console.log("page", page);
-  const dataLength = 200;
-  const limit = 6;
-  const totalPages = Math.ceil(dataLength / limit);
   // Debounced search value
   const debouncedSearch = useDebounceCallback((value: string) => {
     setSearch(value);
@@ -64,30 +53,6 @@ const Blogs = ({ data }: { data: any }) => {
     setSelectedCategory(category);
     updateUrlParams("category", category);
   };
-
-  const handlePageChange = (newPage: number) => {
-    setPage(newPage);
-    updateUrlParams("page", newPage.toString());
-  };
-
-  const handleNext = () => {
-    const nextPage = page + 1;
-    handlePageChange(nextPage);
-  };
-
-  const handlePrevious = () => {
-    const previousPage = page - 1;
-    handlePageChange(previousPage);
-  };
-
-  useEffect(() => {
-    const currentPage = searchParams.get("page");
-    if (currentPage) {
-      handlePageChange(parseInt(currentPage));
-    } else {
-      handlePageChange(1);
-    }
-  }, []);
 
   return (
     <section className="py-12 md:py-28">
@@ -148,83 +113,12 @@ const Blogs = ({ data }: { data: any }) => {
             <BlogCard key={index} article={blog} />
           ))}
 
-          <Pagination className="col-span-full">
-            <PaginationContent className="justify-between w-full">
-              {/* Previous Button */}
-              <PaginationItem
-                onClick={page > 1 ? handlePrevious : undefined}
-                className={cn(
-                  "cursor-pointer",
-                  page === 1 && "cursor-not-allowed opacity-50"
-                )}
-              >
-                <PaginationPrevious />
-              </PaginationItem>
-
-              <div className="hidden md:flex gap-2">
-                {/* First Page */}
-                <PaginationItem
-                  onClick={() => handlePageChange(1)}
-                  className="cursor-pointer"
-                >
-                  <PaginationLink isActive={1 === page}>1</PaginationLink>
-                </PaginationItem>
-
-                {/* Left Ellipsis */}
-                {page > 3 && <PaginationEllipsis />}
-
-                {/* Middle Pages (Dynamically Show Pages Around Current Page) */}
-                {Array.from({ length: totalPages }, (_, index) => index + 1)
-                  .filter(
-                    (pageNumber) =>
-                      pageNumber === page || // Always show the current page
-                      pageNumber === page - 1 || // Show previous page
-                      pageNumber === page + 1 // Show next
-                  )
-                  .map((pageNumber) => (
-                    <PaginationItem
-                      key={pageNumber}
-                      onClick={() => handlePageChange(pageNumber)}
-                      className="cursor-pointer"
-                    >
-                      {pageNumber !== 1 && pageNumber !== totalPages && (
-                        <PaginationLink isActive={pageNumber === page}>
-                          {pageNumber}
-                        </PaginationLink>
-                      )}
-                    </PaginationItem>
-                  ))}
-
-                {/* Right Ellipsis */}
-                {page < totalPages - 2 && <PaginationEllipsis />}
-
-                {/* Last Page */}
-                <PaginationItem
-                  onClick={() => handlePageChange(totalPages)}
-                  className="cursor-pointer"
-                >
-                  <PaginationLink isActive={totalPages === page}>
-                    {totalPages}
-                  </PaginationLink>
-                </PaginationItem>
-              </div>
-
-              <span className="block md:hidden text-gray-9 text-sm font-medium">
-                Page {page} of {totalPages}
-              </span>
-
-              {/* Next Button */}
-              <PaginationItem
-                onClick={page < totalPages ? handleNext : undefined}
-                className={cn(
-                  "cursor-pointer",
-                  page === totalPages && "cursor-not-allowed opacity-50"
-                )}
-              >
-                <PaginationNext />
-              </PaginationItem>
-            </PaginationContent>
-          </Pagination>
+          <PaginationWrapper
+            page={page}
+            setPage={setPage}
+            length={200}
+            className="col-span-full"
+          />
         </div>
       </div>
     </section>
