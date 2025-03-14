674d76232c292a5f5e03ef3a6e3d0d18b1152ccf
Sadman Sakib <ssakib4040@gmail.com>
2025-03-14 12:48:56 +0600

    events ui design created

--- app/(public)/profile/saved-blogs/page.tsx
+++ app/(public)/profile/saved-blogs/page.tsx
@@ -1,5 +1,72 @@
+import Image from "next/image";
 import React from "react";
 
 export default function AccountSavedBlogs() {
-  return <div>AccountSavedBlogs</div>;
+  return (
+    <div className="bg-primary-1 rounded-2xl">
+      <div className="font-semibold lg:text-2xl text-lg lg:py-6 lg:px-8 p-4  bg-primary-2 rounded-t-2xl">
+        Events
+      </div>
+
+      {/* content */}
+      <div className="lg:p-8 p-4">
+        {/* events */}
+        <div className="grid xl:grid-cols-3 lg:grid-cols-2 grid-cols-1 gap-6">
+          {Array(5)
+            .fill(0)
+            .map((_, index) => {
+              return (
+                <div
+                  key={index}
+                  className="bg-primary-2 p-5 rounded-xl relative"
+                >
+                  {/* remove */}
+                  <button className="absolute bg-white flex gap-2 items-center px-6 py-2 right-8 top-8 rounded-xl">
+                    <Image
+                      src="/icons/delete.svg"
+                      alt="close"
+                      width={20}
+                      height={20}
+                    />
+
+                    <span>Remove</span>
+                  </button>
+
+                  <Image
+                    src="https://dummyimage.com/720x480"
+                    alt="event"
+                    height={480}
+                    width={720}
+                    className="rounded-2xl mb-4 w-full"
+                  />
+
+                  {/* time/read min */}
+                  <div className="flex gap-2 items-center justify-between mb-4">
+                    <div className="text-secondary-6 text-base font-semibold">
+                      6th Feb
+                    </div>
+                    <div className="text-secondary-6 text-base font-semibold">
+                      6 minute Read
+                    </div>
+                  </div>
+
+                  {/* title */}
+                  <h2 className="text-gray-900 text-xl font-semibold mb-4">
+                    5 Ways to Build Confidence Your Child with Special Needs
+                  </h2>
+
+                  {/* description */}
+                  <p className="text-gray-900 line-clamp-3">
+                    Empower your child to thrive by fostering self-esteem,
+                    encouraging independence, and celebrating small victories.
+                    Explore practical strategies designed for parents navigating
+                    unique challenges.
+                  </p>
+                </div>
+              );
+            })}
+        </div>
+      </div>
+    </div>
+  );
 }
