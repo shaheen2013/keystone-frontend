d3e16790183a5ce4bc5d9f888ac5d81a800e4467
rasedul <74731905+rasedul99@users.noreply.github.com>
2025-03-18 09:44:16 +0600

    added hero sections for privacy policy and terms and condition page

---
+++ app/(public)/terms-and-condition/constant.ts
@@ -0,0 +1,8 @@
+import heroBgImage from "@/public/assets/terms-and-conditions/hero/image.png";
+
+export const heroData = {
+  title: "Terms & Conditions",
+  description:
+    "Understand the guidelines that govern your use of our services. Your access and engagement with our platform signify your agreement to these terms.",
+  backgroundImage: heroBgImage,
+};
