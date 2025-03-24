e5b037e827b00c98951c309c8ac2d6ca4d8de7f5
rasedul <74731905+rasedul99@users.noreply.github.com>
2025-03-24 12:18:14 +0600

    wip  youtube player glovalize

--- components/partials/WhyKeystone/index.tsx
+++ components/partials/WhyKeystone/index.tsx
@@ -6,6 +6,7 @@ import dynamic from "next/dynamic";
 import { Play } from "@/components/icons";
 import { Button } from "@/components/shadcn/button";
 import { useGetBillingPricesQuery } from "@/features/public/billingSlice";
+import { useState } from "react";
 
 const ReactPlayer = dynamic(() => import("react-player/youtube"), {
   ssr: false,
@@ -17,18 +18,21 @@ const WhyKeystoneSection = ({ data }: { data: any }) => {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const { data: dataGet, error, isLoading } = useGetBillingPricesQuery({});
 
+  const [playing, setPlaying] = useState(false);
+
   return (
     <section className="py-12 md:py-28 bg-primary-2">
       <div className="container grid grid-cols-1 md:grid-cols-2 gap-6 md:gap-12">
         <div className="max-w-[776px] w-full min-h-60 ">
           <ReactPlayer
             url={vedioUrl}
-            loop
-            playIcon={<Play className="size-16 md:size-24 " />}
+            playing={playing}
             controls
             light
             width="100%"
             height="100%"
+            playIcon={<Play className="size-16 md:size-24 " />}
+            onClickPreview={() => setPlaying(true)}
           />
         </div>
         <div className="flex flex-col items-start justify-center text-gray-9">
