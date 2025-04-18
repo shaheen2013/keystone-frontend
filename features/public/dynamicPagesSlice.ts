ffeaf0a2b2693bab5dcf0a2bd7b339d56b5ec737
rasedul <74731905+rasedul99@users.noreply.github.com>
2025-04-18 17:02:30 +0600

    Terms & Conditions and privacy-policy page created

---
+++ features/public/dynamicPagesSlice.ts
@@ -0,0 +1,11 @@
+import { apiSlice } from "../api/apiSlice";
+
+export const dynamicPageSlice = apiSlice.injectEndpoints({
+  endpoints: (builder: any) => ({
+    getDynamicPageContent: builder.query({
+      query: ({ slug }: { slug: string }) => `/pages/${slug}`,
+    }),
+  }),
+});
+
+export const { useGetDynamicPageContentQuery } = dynamicPageSlice;
