5d63ffb1a9bfe1692e43e31533e090be5a92970f
rasedul <74731905+rasedul99@users.noreply.github.com>
2025-04-28 19:00:00 +0600

    add validation to check youtube link

--- components/partials/youtube-player/index.tsx
+++ components/partials/youtube-player/index.tsx
@@ -8,8 +8,24 @@ const ReactPlayer = dynamic(() => import("react-player/youtube"), {
   ssr: false,
 });
 
+// Function to validate YouTube URLs
+const isValidYoutubeUrl = (url: string) => {
+  const patterns = [
+    /^(https?:\/\/)?(www\.)?youtube\.com\/watch\?v=[\w-]+(&\S*)?$/,
+    /^(https?:\/\/)?(www\.)?youtu\.be\/[\w-]+$/,
+    /^(https?:\/\/)?(www\.)?youtube\.com\/embed\/[\w-]+$/,
+    /^(https?:\/\/)?(www\.)?youtube\.com\/v\/[\w-]+$/,
+    /^(https?:\/\/)?(www\.)?youtube\.com\/shorts\/[\w-]+$/,
+  ];
+
+  return patterns.some((pattern) => pattern.test(url));
+};
+
 const YoutubeVideoPlayer = ({ url }: { url: string }) => {
   const [playing, setPlaying] = useState(false);
+
+  if (!url || !isValidYoutubeUrl(url)) return null;
+
   return (
     <>
       <ReactPlayer
