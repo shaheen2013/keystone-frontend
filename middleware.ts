5c02853df20de6ef9abe90d5a33cd1d7d569fb9a
rasedul <74731905+rasedul99@users.noreply.github.com>
2025-04-28 15:03:31 +0600

    token checking layout to middleware

--- middleware.ts
+++ middleware.ts
@@ -2,8 +2,6 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
-  console.log("🔐 Middleware triggered:", request.nextUrl.pathname);
-
   const token = request.cookies.get("key_stone_token")?.value;
 
   if (request.nextUrl.pathname.startsWith("/profile") && !token) {
