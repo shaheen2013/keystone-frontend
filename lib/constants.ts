1c8a4c8ee053937513bd5064e4622616174fce4e
rasedul <74731905+rasedul99@users.noreply.github.com>
2025-05-06 17:26:11 +0600

    update site url

--- lib/constants.ts
+++ lib/constants.ts
@@ -4,6 +4,4 @@ export const TOOLKIT_LIMIT = 4;
 export const USER_EVENTS_LIMIT = 6;
 
 export const SITE_NAME = "KeyStone Ability Support";
-export const SITE_URL =
-  process.env.NEXT_PUBLIC_SITE_URL ||
-  "https://admin-staging.keystoneability.com/api";
+export const SITE_URL = process.env.NEXT_PUBLIC_SITE_URL;
