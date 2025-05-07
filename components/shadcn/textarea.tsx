661c407c01266d736607172f8b82f3000547c5d6
rasedul <74731905+rasedul99@users.noreply.github.com>
2025-05-07 13:56:41 +0600

    refactored input and textarea

--- components/shadcn/textarea.tsx
+++ components/shadcn/textarea.tsx
@@ -13,8 +13,8 @@ const Textarea = React.forwardRef<
     <>
       <textarea
         className={cn(
-          "flex min-h-[60px] w-full rounded-xl border border-gray-2 hover:border-secondary-5 bg-transparent px-4 py-3 text-base shadow-sm placeholder:text-gray-6 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-secondary-5 disabled:cursor-not-allowed disabled:opacity-50",
-          hasError && "border-red-500 focus-visible:ring-red-500",
+          "flex min-h-[60px] w-full rounded-xl border bg-transparent px-4 py-3 text-base shadow-sm placeholder:text-gray-6 focus-visible:outline-none focus-visible:border-primary-4 disabled:cursor-not-allowed disabled:opacity-50",
+          hasError && "border-red-500",
           className
         )}
         ref={ref}
