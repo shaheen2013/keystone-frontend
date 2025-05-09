c8a1c7217b34af2efb745face011b95f5d765faa
rasedul <5777245+shaheen2013@users.noreply.github.com>
2025-05-09 17:57:50 +0600

    added favicon

--- components/shadcn/toast.tsx
+++ components/shadcn/toast.tsx
@@ -29,7 +29,7 @@ const toastVariants = cva(
   {
     variants: {
       variant: {
-        default: "border border-secondary-6 bg-secondary-6 text-white",
+        default: "border bg-background text-foreground",
         destructive:
           "destructive group border-destructive bg-destructive text-destructive-foreground",
       },
